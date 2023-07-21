e1c6abcf15b33d8fd222943c66024bc0796d0355
remmachezoubir <remmachezoubir7@gmail.com>
2023-07-21 19:46:38 +0100

    ok

---
+++ src/components/index.tsx
@@ -0,0 +1,8 @@
+import Profile from "@/components/profile"
+import HomePage from "@/components/home"
+import Friends from "@/components/friends"
+import Notification from "@/components/notification"
+import Settings from "@/components/settings"
+import Header from "@/components/Header"
+
+export {Header , Profile , Friends , HomePage , Notification , Settings}
